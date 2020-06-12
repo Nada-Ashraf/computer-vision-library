b032e8da061d8bdae8cd0754889ecd10b28104eb
nada-ashraf <nadaashraf11@icloud.com>
2020-06-12 19:21:17 +0200

    Implemented nn_interpolate()

--- Homework1:Resizing-and-convolutions/src/resize_image.c
+++ Homework1:Resizing-and-convolutions/src/resize_image.c
@@ -3,14 +3,18 @@
 
 float nn_interpolate(image im, float x, float y, int c)
 {
-    // TODO Fill in
-    return 0;
+    // this function takes any float pixel location
+    // and returns it's the location rounded to the closest int
+    int x_rounded = round(x);
+    int y_rounded = round(y);
+    int pixel_value = get_pixel(im, x_rounded, y_rounded, c);
+    return pixel_value;
 }
 
 image nn_resize(image im, int w, int h)
 {
     // TODO Fill in (also fix that first line)
-    return make_image(1,1,1);
+    return make_image(1, 1, 1);
 }
 
 float bilinear_interpolate(image im, float x, float y, int c)
@@ -22,6 +26,5 @@ float bilinear_interpolate(image im, float x, float y, int c)
 image bilinear_resize(image im, int w, int h)
 {
     // TODO
-    return make_image(1,1,1);
+    return make_image(1, 1, 1);
 }
-
