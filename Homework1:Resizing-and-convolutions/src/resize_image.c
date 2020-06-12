4dcad0a841642bdc5da451a1c4fa78d973d25a50
nada-ashraf <nadaashraf11@icloud.com>
2020-06-12 21:12:00 +0200

    Implemented bilinear interpolation resizing

--- Homework1:Resizing-and-convolutions/src/resize_image.c
+++ Homework1:Resizing-and-convolutions/src/resize_image.c
@@ -44,12 +44,50 @@ image nn_resize(image im, int w, int h)
 
 float bilinear_interpolate(image im, float x, float y, int c)
 {
-    // TODO
-    return 0;
+    // https://github.com/mikeedi/CV-course/blob/master/vision-hw1/src/resize_image.c
+    int floor_x = floor(x);
+    int ceil_x = ceil(x);
+
+    int floor_y = floor(y);
+    int ceil_y = ceil(y);
+
+    float q1 = (y - floor_y) * get_pixel(im, floor_x, ceil_y, c) + (ceil_y - y) * get_pixel(im, floor_x, floor_y, c);
+    float q2 = (y - floor_y) * get_pixel(im, ceil_x, ceil_y, c) + (ceil_y - y) * get_pixel(im, ceil_x, floor_y, c);
+
+    float value = (ceil_x - x) * q1 + (x - floor_x) * q2;
+    return value;
 }
 
 image bilinear_resize(image im, int w, int h)
 {
-    // TODO
-    return make_image(1, 1, 1);
+    // create new image
+    image resized_im = make_image(w, h, im.c);
+
+    // match up cooardinates
+    // given two points (-0.5, -0.5) & (new_w, new_h)
+    // find aX + b = Y coeffiecents
+
+    // x coeffiecents
+    float ax = (float)im.w / w;
+    float bx = 0.5 * ax - 0.5;
+    // y coeffiecents
+    float ay = (float)im.h / h;
+    float by = 0.5 * ay - 0.5;
+
+    // iterate over new points and map to old coords
+    for (int x = 0; x < w; x++)
+    {
+        for (int y = 0; y < h; y++)
+        {
+            for (int c = 0; c < im.c; c++)
+            {
+                float old_x = x * ax + bx;
+                float old_y = y * ay + by;
+                float interpolated_value = bilinear_interpolate(im, old_x, old_y, c);
+                set_pixel(resized_im, x, y, c, interpolated_value);
+            }
+        }
+    }
+
+    return resized_im;
 }
--- Homework1:Resizing-and-convolutions/test.py
+++ Homework1:Resizing-and-convolutions/test.py
@@ -1,5 +1,11 @@
 from uwimg import *
 
+# 1. Resizing using nearest neighbour interpolation
 im = load_image("data/dogsmall.jpg")
 a = nn_resize(im, im.w*5, im.h*5)
 save_image(a, "./results/dog4x-nn")
+
+# 1. Resizing using bilinear interpolation
+im = load_image("data/dogsmall.jpg")
+a = bilinear_resize(im, im.w*4, im.h*4)
+save_image(a, "./results/dog4x-bl")
